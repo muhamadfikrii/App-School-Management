7d54c3efdcf39994da4448d6455612a76a1a98cd
Soleman Zoya <solemanzoyaa@gmail.com>
2025-09-14 19:38:40 +0700

    Fixs bugs enums

--- app/Filament/Resources/ClassRombel/ClassRombelResource/RelationManagers/StudentsRelationManager.php
+++ app/Filament/Resources/ClassRombel/ClassRombelResource/RelationManagers/StudentsRelationManager.php
@@ -13,42 +13,4 @@ class StudentsRelationManager extends RelationManager
     protected static string $relationship = 'student';
 
     protected static ?string $relatedResource = StudentsResource::class;
-
-    public function table(Table $table): Table
-    {
-        return $table
-            ->columns([
-                TextColumn::make('nisn')
-                    ->searchable()
-                    ->label('NISN'),
-                TextColumn::make('full_name')
-                    ->searchable()
-                    ->label('Nama Lengkap'),
-                TextColumn::make('date_of_birth')
-                    ->date(' d M Y')
-                    ->searchable()
-                    ->label('Tanggal Lahir')
-                    ->toggleable(isToggledHiddenByDefault: true),
-                TextColumn::make('gender')
-                    ->searchable(),
-                TextColumn::make('classRombel.name')
-                    ->label('Kelas')
-                    ->searchable(),
-                TextColumn::make('year_enrollment')
-                    ->label('Tahun Masuk')
-                    ->searchable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                TextColumn::make('created_at')
-                    ->label('Dibuat')
-                    ->searchable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                TextColumn::make('updated_at')
-                    ->label('Diubah')
-                    ->searchable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-            ])
-            ->headerActions([
-                CreateAction::make(),
-            ]);
-    }
 }
