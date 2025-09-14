d47a18cafde5c58807f7b8acef6cb3a0b2e7f02f
Soleman Zoya <solemanzoyaa@gmail.com>
2025-09-15 01:27:14 +0700

    Formatting

--- app/Filament/Resources/ClassRombel/Schemas/ClassRombelInfolist.php
+++ app/Filament/Resources/ClassRombel/Schemas/ClassRombelInfolist.php
@@ -16,26 +16,25 @@ class ClassRombelInfolist
             ->columns(3)
             ->components([
                 Section::make("Kelas")
-                ->columnSpan(2)
-                    ->schema([
-                        Grid::make(2)
-                            ->schema([
-                                TextEntry::make("name")
-                                    ->label("Nama Kelas"),
-                                TextEntry::make('teacher_id')
-                                    ->label('Wali Kelas')
-                                    ->formatStateUsing(fn($state, $record): string => $record->teacher ? $record->teacher->full_name : '-' ),
-                                TextEntry::make('level.name')
-                                    ->label('Tingkat Kelas'),
-                                TextEntry::make('major.name')
-                                    ->label('Jurusan'),
-                                
+                    ->columnSpan(2)
+                        ->schema([
+                            Grid::make(2)
+                                ->schema([
+                                    TextEntry::make("name")
+                                        ->label("Nama Kelas"),
+                                    TextEntry::make('teacher_id')
+                                        ->label('Wali Kelas')
+                                        ->formatStateUsing(fn($state, $record): string => $record->teacher ? $record->teacher->full_name : '-' ),
+                                    TextEntry::make('level.name')
+                                        ->label('Tingkat Kelas'),
+                                    TextEntry::make('major.name')
+                                        ->label('Jurusan'),
                                 ]),
-                            ]),
-                            Section::make()
+                        ]),
+                        Section::make()
                             ->schema([
                                 ImageEntry::make('teacher.avatar_url')
-                                ])
-                ]);
+                            ])
+                    ]);
     }
 }
