8ef71b888efb867b68eb39957c144651e0afe2c2
muhamadfikrii <muhamadfikriardiansyah95@gmail.com>
2025-09-19 22:35:04 +0700

    remove

--- resources/views/emails/invitation.blade.php
+++ resources/views/emails/invitation.blade.php
@@ -1,13 +1,11 @@
 @component('mail::message')
 # Hello {{ $invite_name }},
+
 <span>
 {{ $inviter_name }} dari SMKN 4 KUNINGAN telah mengundang anda.
 Silahkan klik tombol di bawah ini. 
 tautan ini akan kadaluarsa dalam 2 jam, Hubungi kami jika anda memerlukan tautan baru.
 </span>
-
-<img style="max-width: 100px; height: 100px; display: block; margin: 0 auto;" 
-src="{{ asset('img/logo.png') }}" alt="logo.png">
 
 @component('mail::button', ['url' => url($accept_url)])
 Accept Invitation
